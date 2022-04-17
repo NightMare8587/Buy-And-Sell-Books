dddf3b1dae878a63f498840bf266510d11ca7fc6
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-04-17 11:55:57 +0530

    Update GoogleSignInDB.java

--- app/src/main/java/com/consumers/librarymanagementsystem/GoogleSignInDB.java
+++ app/src/main/java/com/consumers/librarymanagementsystem/GoogleSignInDB.java
@@ -2,7 +2,6 @@ package com.consumers.librarymanagementsystem;
 
 public class GoogleSignInDB {
     public String name,email;
-
     public GoogleSignInDB(String name, String email) {
         this.name = name;
         this.email = email;
